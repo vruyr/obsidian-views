720ed7dc2f68570a3b26a7ccd5c76fa77fe1920e
Vruyr Gyolchanyan <vruyr@vruyr.com>
2023-07-29 22:00:53 -0400

    Changed "Book Info" view to use date based progress data fields instead of "Started Reading" and "Finished Reading".

---
+++ Book Info/lib.js
@@ -0,0 +1,124 @@
+const READING_SPEEDS_WPM = [150, 300];
+const CALIBRE_LIBRARY = "Calibre";
+const PROGRESS_LOG_KEY_REGEX = /^\d{4}-\d{2}-\d{2}$/;
+const PROGRESS_LOG_VAL_REGEX = /^\s*(?:(?:on\s*)?page)?\s*(?<page>\d+)\s*\/\s*(?<total>\d+)(?:(?!\d)[\s-:]*(?<note>.*))?\s*$/i;
+
+function makeArray(o) {
+	return Array.isArray(o) ? o : [o]
+}
+
+function ProgressLog(params) {
+	Object.assign(this, params);
+}
+
+ProgressLog.prototype.percentage = function() {
+	return this.page / this.total;
+}
+
+ProgressLog.prototype.pctstr = function() {
+	return this.percentage().toLocaleString(undefined, {style: "percent", minimumFractionDigits: 1, maximumFractionDigits: 1});
+}
+
+const fancyArrayPrototype = {
+	get last() {
+		return this[this.length - 1];
+	}
+};
+
+Object.setPrototypeOf(fancyArrayPrototype, Array.prototype);
+
+function makeFancyArray() {
+	let result = [...arguments];
+	Object.setPrototypeOf(result, fancyArrayPrototype);
+	return result;
+}
+
+function getProgressLogs(page) {
+	const resultFlat = Object.entries(
+		page
+	).flatMap(([k, v]) => {
+		if(!PROGRESS_LOG_KEY_REGEX.test(k)) {
+			return [];
+		}
+		k = dv.date(k);
+		v = makeArray(v).flatMap(x => {
+			const m = PROGRESS_LOG_VAL_REGEX.exec(x);
+			if(!m) {
+				return [];
+			}
+			return m.groups;
+		});
+		if(!v.length) {
+			return [];
+		}
+		v = v.reduce((a, b) => ((a.page/a.total) < (b.page/b.total) ? b : a));
+		return [new ProgressLog(Object.assign({date: k}, v))];
+	}).sort((a, b) => (
+		a.date.valueOf() - b.date.valueOf() || a.percentage() - b.percentage()
+	));
+
+	if(resultFlat.length < 2) {
+		return [resultFlat]
+	}
+
+	const result = makeFancyArray(
+		makeFancyArray(resultFlat[0]),
+	);
+
+	for(let i = 1; i < resultFlat.length; i++) {
+		if(resultFlat[i - 1].percentage() > resultFlat[i].percentage()) {
+			result.push(makeFancyArray());
+		}
+		result[result.length - 1].push(resultFlat[i]);
+	}
+
+	return result;
+}
+
+function isObsidianMobile() {
+	// Incidentally, in the mobile app, the Buffer API is not available.
+	return typeof this["Buffer"] === "undefined";
+}
+
+function formatDate(d) {
+	return d.toFormat(dv.settings.defaultDateFormat);
+}
+
+function formatDuration(d) {
+	return d.shiftTo("days").toHuman({listStyle: "short", unitDisplay: "short"});
+}
+
+function formatDateRange(start, end, options) {
+	options = Object.assign({
+		showRelativeStart: false,
+		showRelativeEnd: false
+	}, options);
+
+	if(start.valueOf() === end.valueOf()) {
+		return `on ${formatDate(start)} (${start.toRelativeCalendar({unit: "days"})})`;
+	}
+
+	let result = [
+		"in ", formatDuration(end.diff(start)),
+		" from ", formatDate(start),
+	];
+
+	if(options.showRelativeStart) {
+		result.push(` (${start.toRelativeCalendar({unit: "days"})})`);
+	}
+	result.push(" to ");
+	result.push(formatDate(end));
+
+	if(options.showRelativeEnd) {
+		result.push(` (${end.toRelativeCalendar({unit: "days"})})`);
+	}
+	return result.join("");
+}
+
+return {
+	READING_SPEEDS_WPM,
+	CALIBRE_LIBRARY,
+	getProgressLogs,
+	isObsidianMobile,
+	formatDateRange,
+}
--- Book Info/view.js
+++ Book Info/view.js
@@ -1,5 +1,10 @@
-const READING_SPEEDS_WPM = [150, 300];
-const CALIBRE_LIBRARY = "Calibre";
+const lib = (new Function(
+	"dv",
+	//TODO Figure out how to make relative paths work for Dataview "view"s.
+	await dv.io.load("Views/Book Info/lib.js"))
+)(
+	dv
+);
 
 const page = dv.current();
 const book = page.book;
@@ -25,8 +30,8 @@ if(statsPages) {
 }
 if(statsWords) {
 	summaryTable.push(["Total words", statsWords.toLocaleString()])
-	const speedMin = Math.min(...READING_SPEEDS_WPM);
-	const speedMax = Math.max(...READING_SPEEDS_WPM);
+	const speedMin = Math.min(...lib.READING_SPEEDS_WPM);
+	const speedMax = Math.max(...lib.READING_SPEEDS_WPM);
 	const timeMin = Math.floor(statsWords / (speedMax * 60));
 	const timeMax = Math.ceil(statsWords / (speedMin * 60))
 	summaryTable.push(["Time to read",
@@ -36,49 +41,44 @@ if(statsWords) {
 
 summaryTable.push(["---", "---"]);
 
-// {label: {("start"|"finish"): timestamp_string|[timestamp_string]}}
-const readingTimeline = Object.getOwnPropertyNames(
-	page
-).map(k => {
-	const m = /^(Start|Finish)ed Reading\s*(.*)/.exec(k);
-	if(m == null) {
-		return null;
+let progressLogs = lib.getProgressLogs(page).last;
+
+if(progressLogs.length) {
+	const first = progressLogs[0];
+	const last = progressLogs[progressLogs.length - 1];
+	const pct = last.percentage();
+	let wordsPerDay = null;
+	if(pct && statsWords) {
+		wordsPerDay = statsWords * last.percentage() / (last.date.diff(first.date).shiftTo("days").days);
 	}
-	return [m[1], m[2], page[k]];
-}).filter(
-	x => x
-).reduce(
-	((result, [startOrFinish, label, date]) => {
-		if(!result.hasOwnProperty(label)) {
-			result[label] = {start: null, finish:null};
-		}
-		result[label][startOrFinish.toLowerCase()] = Array.isArray(date) ? date : [date];
-		return result;
-	}),
-	{}
-);
-
-for(const [label, times] of Object.entries(readingTimeline)) {
-	const formatDate = d => d.toLocaleString({dateStyle: "medium"});
-	const formatDuration = d => d.shiftTo("days").toHuman({listStyle: "short", unitDisplay: "short"})
-
-	times.start?.sort();
-	times.finish?.sort();
-
-	while(times.start?.length) {
-		const timeStart = times.start.shift();
-		const timeFinish = times.finish?.length ? times.finish.shift() : null;
-		if(timeStart && timeFinish) {
-			summaryTable.push([`Read: ${label}`,
-				`${formatDate(timeStart)} to ${formatDate(timeFinish)} (${formatDuration(timeFinish.diff(timeStart))}; ${timeFinish.toRelativeCalendar({unit: "days"})})`
-			])
-		} else if(timeStart) {
-			summaryTable.push([`Started reading: ${label}`, `${timeStart.toRelativeCalendar({unit: "days"})}`]);
-		} else if(timeFinish){
-			summaryTable.push([`Finished reading: ${label}`,
-				`${formatDate(timeFinish)} (${timeFinish.toRelativeCalendar({unit: "days"})})`
-			]);
-		}
+	let wordsPerDayMsg = "";
+	if(wordsPerDay) {
+		wordsPerDayMsg = `\nOn average ${wordsPerDay.toLocaleString(undefined, {maximumFractionDigits: 0})} words per day`;
+	}
+	if(last.page == last.total) {
+		summaryTable.push([
+			"Last read",
+			(
+				lib.formatDateRange(first.date, last.date, {
+					showRelativeEnd: true,
+				})
+				+
+				wordsPerDayMsg
+			)
+		]);
+	} else {
+		let note = last.note ? ` - ${last.note}` : "";
+		summaryTable.push([
+			`Read to ${last.pctstr()}${note}`,
+			(
+				lib.formatDateRange(first.date, last.date, {
+					showRelativeStart: false,
+					showRelativeEnd: true,
+				})
+				+
+				wordsPerDayMsg
+			)
+		]);
 	}
 }
 
@@ -88,16 +88,11 @@ dv.table(["", ""], summaryTable);
 
 const references = [];
 
-function isObsidianMobile() {
-	// Incidentally, in the mobile app, the Buffer API is not available.
-	return typeof this["Buffer"] === "undefined";
-}
-
-if(!isObsidianMobile()) {
+if(!lib.isObsidianMobile()) {
 	//TODO:vruyr Rethink the Calibre search. Perhaps search all the identifier of all editions.
 	const firstId = bookEditionSelected.identifiers[0];
 	const calibreSearch = `identifiers:"=${firstId.type}:${firstId.id}"`;
-	const l = new Buffer(CALIBRE_LIBRARY).toString("hex");
+	const l = new Buffer(lib.CALIBRE_LIBRARY).toString("hex");
 	const q = new Buffer(calibreSearch).toString("hex");
 	references.push(`- [Open in Calibre](calibre://search/_hex_-${l}?eq=${q})`);
 }
