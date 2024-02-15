254dab206706dac76caf56869c16798e11f5c0d4
Vruyr Gyolchanyan <vruyr@vruyr.com>
2024-02-15 18:56:22 -0500

    Added an H1 heading to the daily note.

--- Views in Daily Notes/view.js
+++ Views in Daily Notes/view.js
@@ -1,9 +1,31 @@
 async function main() {
+	const currentPage = await waitForCurrentPage();
+	const currentPageDate = moment(currentPage.file.name, "YYYY-MM-DD");
+
+	// https://momentjs.com/docs/#/displaying/format/
+	const DAILY_NOTE_PAGE_TITLE_MOMENT_FORMAT = "dddd gg[W]ww, MMMM D, YYYY";
+
+	dv.paragraph(`# ${currentPageDate.format(DAILY_NOTE_PAGE_TITLE_MOMENT_FORMAT)}`);
+
 	dv.paragraph("## Pending");
 	await dv.view("Views/Pending Tasks in Daily Notes");
 	dv.paragraph("&nbsp;");
 	dv.paragraph("## References");
 	await dv.view("Views/References");
+}
+
+async function waitForCurrentPage() {
+	let currentPage;
+	while(!(currentPage = dv.current())) {
+		await sleep(100);
+	}
+	return currentPage;
+}
+
+async function sleep(milliseconds) {
+	await new Promise(resolve => {
+		setTimeout(resolve, milliseconds);
+	});
 }
 
 try {
