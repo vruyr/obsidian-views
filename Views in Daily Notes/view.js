c80d4a1e893eeb3b94f043ce1b5c6cde9a6dcd05
Vruyr Gyolchanyan <vruyr@vruyr.com>
2024-03-03 18:07:53 -0500

    Updated the daily note's title.

--- Views in Daily Notes/view.js
+++ Views in Daily Notes/view.js
@@ -3,9 +3,10 @@ async function main() {
 	const currentPageDate = moment(currentPage.file.name, "YYYY-MM-DD");
 
 	// https://momentjs.com/docs/#/displaying/format/
-	const DAILY_NOTE_PAGE_TITLE_MOMENT_FORMAT = "dddd gg[W]ww, MMMM D, YYYY";
+	const dayByWeek = currentPageDate.format("gg[W]wwdd").toUpperCase();
+	const dayByMonth = currentPageDate.format("MMMM D, YYYY")
 
-	dv.paragraph(`# ${currentPageDate.format(DAILY_NOTE_PAGE_TITLE_MOMENT_FORMAT)}`);
+	dv.paragraph(`# ${dayByWeek} – ${dayByMonth}`);
 
 	dv.paragraph("## Pending");
 	await dv.view("Views/Pending Tasks in Daily Notes");
